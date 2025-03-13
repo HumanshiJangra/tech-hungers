49d14fbb0037b3da23438d0a40d71b7f316a3092
HumanshiJangra <humanshijangra123@gmail.com>
2025-03-13 22:33:02 +0530

    hover buttons

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -4,16 +4,16 @@ import Link from 'next/link';
 interface ButtonProps {
     text: string;
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
-    href?: string; // Optional prop to determine if it's a link
+    href?: string; 
     className?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ text, onClick, href, className }) => {
-    const baseStyles = "flex items-center gap-2.5 bg-[radial-gradient(at_top_left,_#15172a_0%,_#353c71_65%)] text-white text-base px-8 py-3 text-center rounded-[15px]";
-    
+    const baseStyles = "hover:bg-[radial-gradient(at_top_left,_#fff_0%,_#fff_65%)] hover:text-[#16172b] hover:shadow-[-1px_1px_6px_2px_#8f919e] flex items-center gap-2.5 bg-[radial-gradient(at_top_left,_#15172a_0%,_#353c71_65%)] text-white text-base px-8 py-3 text-center rounded-[15px]  ";
+
     if (href) {
         return (
-            <Link href={href} className={`${baseStyles} ${className}`}>
+            <Link href={href} className={`${baseStyles} ${className}  `}>
                 {text}
             </Link>
         );
