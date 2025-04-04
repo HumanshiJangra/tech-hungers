ff423bf2d49e2ec89ea649d4c0288bb41ac59e40
HumanshiJangra <humanshijangra123@gmail.com>
2025-04-04 11:41:24 +0530

    revert

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,24 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
 import type { NextConfig } from "next";
- 
+
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/(.*)",
-        destination: "https://techhungers.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
- 
+
 export default nextConfig;
+// import type { NextConfig } from "next";
+ 
+// const nextConfig: NextConfig = {
+//   async redirects() {
+//     return [
+//       {
+//         source: "/(.*)",
+//         destination: "https://techhungers.com/:path*",
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+ 
+// export default nextConfig;
  
  
